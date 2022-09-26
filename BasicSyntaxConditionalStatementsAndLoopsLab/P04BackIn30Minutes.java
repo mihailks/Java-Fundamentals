60c09e3c44a1c643bce92ebcbd09e827702948e4
Mihail Slavov <inj.slavov@gmail.com>
2022-09-26 16:53:24 +0300

    -

--- BasicSyntaxConditionalStatementsAndLoopsLab/P04BackIn30Minutes.java
+++ BasicSyntaxConditionalStatementsAndLoopsLab/P04BackIn30Minutes.java
@@ -1,7 +1,21 @@
 package BasicSyntaxConditionalStatementsAndLoopsLab;
 
+import java.util.Scanner;
+
 public class P04BackIn30Minutes {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int hours = Integer.parseInt(scanner.nextLine());
+        int minutes = Integer.parseInt(scanner.nextLine());
+
+        int totalTime = hours * 60 + minutes + 30;
+        int newHours = totalTime / 60;
+        int newMinutes = totalTime % 60;
+
+        if (newHours == 24) {
+            newHours = 0;
+        }
+        System.out.printf("%d:%02d", newHours, newMinutes);
+
     }
 }
