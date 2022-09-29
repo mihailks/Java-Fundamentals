e63241147af9edba3bea06fdb6004980fdac4607
Mihail Slavov <inj.slavov@gmail.com>
2022-09-29 14:36:31 +0300

    -

---
+++ BasicSyntaxConditionalStatementsAndLoopsExercise/P02Division.java
@@ -0,0 +1,12 @@
+package BasicSyntaxConditionalStatementsAndLoopsExercise;
+
+import java.util.Scanner;
+
+public class P02Division {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+}
