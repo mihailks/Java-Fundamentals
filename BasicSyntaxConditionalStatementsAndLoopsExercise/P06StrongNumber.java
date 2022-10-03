0fd68eb8fc6f1e91f8cda276e2e8026badfec87c
Mihail Slavov <mihail_slavov@icloud.com>
2022-10-03 20:10:20 +0300

    new

---
+++ BasicSyntaxConditionalStatementsAndLoopsExercise/P06StrongNumber.java
@@ -0,0 +1,34 @@
+package BasicSyntaxConditionalStatementsandLoopsExercise;
+
+import java.util.Scanner;
+
+public class P06StrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int check = n;
+        int temp = 0;
+        int currentTotal = 1;
+        int total = 0;
+
+        while (n > 0) {
+            temp = n % 10;
+            if (temp != 0) {
+                for (int i = temp; i >= 1; i--) {
+                    currentTotal *= i;
+                }
+                total += currentTotal;
+                currentTotal = 1;
+            } else {
+                total += 1;
+            }
+            n /= 10;
+        }
+        if (total == check) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
---
+++ BasicSyntaxConditionalStatementsAndLoopsExercise/P07VendingMachine.java
@@ -0,0 +1,61 @@
+package BasicSyntaxConditionalStatementsandLoopsExercise;
+
+import com.sun.source.tree.WhileLoopTree;
+
+import java.util.Scanner;
+
+public class P07VendingMachine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        double currentMoney = 0;
+        double totalMoney = 0;
+        double moneyLeft = 0;
+        while (!input.equals("Start")) {
+
+            currentMoney = Double.parseDouble(input);
+            if (currentMoney == 0.1 || currentMoney == 0.2 || currentMoney == 0.5 || currentMoney == 1) {
+                totalMoney += currentMoney;
+            } else {
+                System.out.printf("Cannot accept %.2f%n", currentMoney);
+            }
+            input = scanner.nextLine();
+        }
+        double price = 0;
+        input=scanner.nextLine();
+        while (!input.equals("End")) {
+            switch (input) {
+                case "Nuts":
+                    price = 2;
+                    break;
+                case "Water":
+                    price = 0.7;
+                    break;
+                case "Crisps":
+                    price = 1.5;
+                    break;
+                case "Soda":
+                    price = 0.8;
+                    break;
+                case "Coke":
+                    price = 1;
+                    break;
+                default:
+                    System.out.println("Invalid product");
+            }
+            if (price <= totalMoney && totalMoney!=0) {
+                System.out.println("Purchased " + input);
+                totalMoney -= price;
+            } else {
+                System.out.println("Sorry, not enough money");
+            }
+            price = 0;
+
+            input = scanner.nextLine();
+        }
+
+        if (totalMoney >= 0) {
+            System.out.printf("Change: %.2f", totalMoney);
+        }
+    }
+}
