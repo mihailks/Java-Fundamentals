da8db740da309d45963b687d77e1e56c323bddb0
Mihail Slavov <inj.slavov@gmail.com>
2022-10-06 12:52:10 +0300

    Array

--- ArraysExercise/P05TopIntegers.java
+++ ArraysExercise/P05TopIntegers.java
@@ -1,9 +1,25 @@
 package ArraysExercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P05TopIntegers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int[] myArr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        for (int i = 0; i < myArr.length; i++) {
+            boolean biggest = true;
+            for (int j = i+1; j < myArr.length; j++) {
+                if (myArr[i] <= myArr[j]) {
+                    biggest = false;
+                    break;
+                }
+            }
+            if (biggest) {
+                System.out.print(myArr[i] + " ");
+            }
+        }
     }
 }
--- ArraysExercise/P06EqualSums.java
+++ ArraysExercise/P06EqualSums.java
@@ -1,9 +1,33 @@
 package ArraysExercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P06EqualSums {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int[] myArr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        boolean found = false;
+        for (int i = 0; i < myArr.length; i++) {
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int j = 0; j < i; j++) {
+                leftSum += myArr[j];
+            }
+            for (int j = i + 1; j < myArr.length; j++) {
+                rightSum += myArr[j];
+            }
+            if (leftSum == rightSum) {
+                found = true;
+                System.out.println(i);
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("no");
+        }
     }
 }
