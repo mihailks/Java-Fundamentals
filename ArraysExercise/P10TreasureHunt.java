314fa2785ef3caa3cad74360ecff4a1f21b12cad
Mihail Slavov <inj.slavov@gmail.com>
2022-10-12 13:26:15 +0300

    there is something wrong....

--- ArraysExercise/P10TreasureHunt.java
+++ ArraysExercise/P10TreasureHunt.java
@@ -1,5 +1,6 @@
 package ArraysExercise;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class P10TreasureHunt {
@@ -10,7 +11,7 @@ public class P10TreasureHunt {
         String[] chest = scanner.nextLine().split("\\|");
 
         String[] loot = scanner.nextLine().split(" ");
-        String temp = "";
+
         while (!loot[0].equals("Yohoho!")) {
             switch (loot[0]) {
                 case "Loot":
@@ -20,36 +21,53 @@ public class P10TreasureHunt {
                         for (int j = 1; j < loot.length; j++) {
                             if (chest[i].equals(loot[j])) {
                                 sameItem = true;
-                                indexSameItem = i;
+                                indexSameItem = j;
                                 break;
                             }
                         }
+                        if (sameItem) {
+                            break;
+                        }
                     }
+                    String[] tempChest = new String[loot.length - 2];
                     if (sameItem) {
-                        String[] tempChest = new String[chest.length - 1];
-                        for (int i = 0; i < chest.length - 1; i++) {
+                        for (int i = 1, j = 0; i < loot.length; i++) {
                             if (i != indexSameItem) {
-                                tempChest[i] = chest[i];
+                                tempChest[j++] = loot[i];
                             }
                         }
-                        chest = tempChest;
+                        loot = tempChest;
                     }
-                    String[] tempChest = new String[chest.length + loot.length];
-                    for (int i = loot.length - 1; i > 0; i--) {
+                    tempChest = new String[chest.length + loot.length];
+                    for (int i = loot.length - 1; i >= 0; i--) {
                         tempChest[i] = loot[i];
                     }
-                    for (int i = 0; i < chest.length; i++) {
-                        tempChest[i + loot.length - 1] = chest[i];
+                    for (int i = loot.length; i < tempChest.length; i++) {
+                        tempChest[i] = chest[i - loot.length];
                     }
-                    break;
+                    chest = tempChest;
 
+
+                    break;
                 case "Drop":
-                    if (Integer.parseInt(loot[1]) > chest.length) {
+                    if (Integer.parseInt(loot[1]) >= chest.length || Integer.parseInt(loot[1]) < 0) {
+                        loot = scanner.nextLine().split(" ");
                         continue;
                     } else {
+                        String temp = "";
+                        tempChest = new String[chest.length];
                         temp = chest[Integer.parseInt(loot[1])];
-                        chest[Integer.parseInt(loot[1])] = chest[chest.length - 1];
-                        chest[chest.length - 1] = temp;
+                        for (int i = 0, j = 0; i < chest.length; i++) {
+                            if (i != Integer.parseInt(loot[1])) {
+                                tempChest[j++] = chest[i];
+                            }
+                        }
+                        tempChest[tempChest.length-1]=temp;
+                        chest=tempChest;
+//                        String temp = "";
+//                        temp = chest[Integer.parseInt(loot[1])];
+//                        chest[Integer.parseInt(loot[1])] = chest[chest.length - 1];
+//                        chest[chest.length - 1] = temp;
                     }
                     break;
                 case "Steal":
@@ -57,23 +75,31 @@ public class P10TreasureHunt {
                         loot[1] = String.valueOf(chest.length);
                     }
                     int length = chest.length - Integer.parseInt(loot[1]);
+                    String[] finalChest = new String[chest.length-length];
                     for (int i = length; i < chest.length; i++) {
                         if (i == chest.length - 1) {
                             System.out.print(chest[i]);
                         } else {
                             System.out.print(chest[i] + ", ");
                         }
+                        finalChest[i-length]=chest[i];
                     }
+                    chest=finalChest;
                     break;
             }
             loot = scanner.nextLine().split(" ");
         }
-
-
-        str.append(chest[0]);
-        for (int j = 1; j < chest.length; j++) {
-            str.append(", ").append(chest[j]);
+        System.out.println();
+        String treasureCount = String.join("", chest);
+        int charCounter = 0;
+        for (int i = 0; i < treasureCount.length(); i++) {
+            charCounter++;
         }
-        System.out.println(str);
+        double averageTreasure = (1.0 * charCounter / chest.length);
+        if (charCounter > 0) {
+            System.out.printf("Average treasure gain: %.2f pirate credits.", averageTreasure);
+        } else {
+            System.out.println("Failed treasure hunt.");
+        }
     }
 }
