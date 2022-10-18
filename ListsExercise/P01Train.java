a2bf3f5ebe35e70beba1e4d92e163718155158a5
Mihail Slavov <inj.slavov@gmail.com>
2022-10-18 16:55:03 +0300

    -

--- ListsExercise/P01Train.java
+++ ListsExercise/P01Train.java
@@ -1,11 +1,39 @@
 package ListsExercise;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class P01Train {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        List<Integer> peopleInWagons = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        String[] numberOfPeople = scanner.nextLine().split(" ");
 
+
+        while (!numberOfPeople[0].equals("end")) {
+            if (numberOfPeople[0].equals("Add")) {
+                peopleInWagons.add(Integer.parseInt(numberOfPeople[1]));
+
+
+            } else {
+
+                int peopleToAdd = Integer.parseInt(numberOfPeople[0]);
+
+                for (int i = 0; i < peopleInWagons.size(); i++) {
+                    if (peopleInWagons.get(i) - peopleToAdd <= 0) {
+                        peopleInWagons.set(i, peopleInWagons.get(i) + peopleToAdd);
+                    }
+                }
+
+            }
+            numberOfPeople = scanner.nextLine().split(" ");
+        }
+
+        System.out.println(peopleInWagons.toString().replaceAll("[\\[\\],]",""));
     }
 }
