7c684c971044acc0f6d0375922de545e719722f8
Mihail Slavov <mihail_slavov@icloud.com>
2022-10-22 11:14:51 +0300

    to do

--- ArraysMoreExercise/P05KaminoFactory.java
+++ ArraysMoreExercise/P05KaminoFactory.java
@@ -1,48 +1,64 @@
-package ArraysMoreExercise;
-
-import java.util.Scanner;
-
-public class P05KaminoFactory {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        String input = scanner.nextLine();
-        int sum = 0;
-        int maxSum=0;
-        int firstIndex=0;
-        int iCounter= 1;
-        int sampleNumber=0;
-        while (!input.equals("Clone them!")){
-            for (int i = 0; i < input.length()-1; i++) {
-                if (Character.isDigit(input.charAt(i))){
-
-                    sum+=input.charAt(i);
-                    if (input.charAt(i)==1){
-                        sum++;
-                    }
-
-                    if (input.charAt(i)==1 && input.charAt(i+1)==1){
-                        iCounter++;
-                    } else {
-
-                    }
-                }
-            }
-            if (sum>maxSum){
-                maxSum=sum;
-                sum=0;
-            }
-            input = scanner.nextLine();
-        }
-        String output = "";
-        System.out.printf("Best DNA sample %d with sum: ",sampleNumber);
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i)=='!'){
-                output+=" ";
-            }else {
-                output+=input.charAt(i);
-            }
-        }
-        System.out.printf("%s.",output);
-    }
-}
+//package ArraysExercise;
+//
+//import java.util.Scanner;
+//
+//public class P09ArrayModifier {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int n = Integer.parseInt(scanner.nextLine());
+//
+//        String line = scanner.nextLine();
+//
+//        int bestDnaPosition = 0;
+//        int maxLength = 0;
+//        int maxSumElements = 0;
+//        String bestLocalDna = "";
+//        int countDna = 0;
+//        int bestCountDna = 0;
+//        String[] bestDna = new String[n];
+//        while (!line.equals("Clone them!")) {
+//            countDna++;
+//            String data = line.replaceAll("!", "");
+//            String[] dna = data.split("0");
+//            int currentLength = 0;
+//            int currentSumElement = 0;
+//            int currentPosition = 0;
+//
+//
+//            for (int i = 0; i < dna.length; i++) {
+//                currentSumElement += dna[i].length();
+//                if (dna[i].length() > currentLength) {
+//                    currentLength = dna[i].length();
+//                    bestLocalDna = dna[i];
+//
+//                }
+//            }
+//            currentPosition = data.indexOf(bestLocalDna);
+//
+//            if (currentLength > maxLength) {
+//                maxLength = currentLength;
+//                maxSumElements = currentSumElement;
+//                bestDnaPosition = currentPosition;
+//                bestCountDna = countDna;
+//                bestDna = data.split("");
+//            } else if (currentLength == maxLength && (bestDnaPosition > currentPosition ||
+//                    currentSumElement > maxSumElements)) {
+//                maxLength = currentSumElement;
+//                maxSumElements = currentSumElement;
+//                bestDnaPosition = currentPosition;
+//                bestCountDna = countDna;
+//                bestDna = data.split("");
+//            } else if (countDna == 1) {
+//                maxLength = currentSumElement;
+//                maxSumElements = currentSumElement;
+//                bestDnaPosition = currentPosition;
+//                bestCountDna = countDna;
+//                bestDna = data.split("");
+//            }
+//            line = scanner.nextLine();
+//        }
+//        System.out.printf("Best DNA sample %d with sum: %d\n", bestCountDna, maxSumElements);
+//        System.out.println(String.join(" ", bestDna));
+//    }
+//}
