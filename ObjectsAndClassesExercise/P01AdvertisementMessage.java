fd0e375d97a65ea151e49d46e9a9806b5d41d316
Mihail Slavov <inj.slavov@gmail.com>
2022-11-14 16:58:08 +0200

    yep

--- ObjectsAndClassesExercise/P01AdvertisementMessage.java
+++ ObjectsAndClassesExercise/P01AdvertisementMessage.java
@@ -1,4 +1,43 @@
 package ObjectsAndClassesExercise;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class P01AdvertisementMessage {
+
+
+    public static class Message {
+        private Random random = new Random();
+
+        private String[] phrases = {"Excellent product.", "Such a great product.",
+                "I always use that product.", "Best product of its category."
+                , "Exceptional product.", "I can’t live without this product."};
+        private String[] events = {"Now I feel good.", "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.", "I feel great!"};
+        private String[] author = {"Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"};
+        private String[] town = {"Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"};
+
+
+        public String randomMessage() {
+            return String.format("%s %s %s - %s",
+                    phrases[random.nextInt(phrases.length)],
+                    events[random.nextInt(events.length)],
+                    author[random.nextInt(author.length)],
+                    town[random.nextInt(town.length)]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Message message = new Message();
+
+        for (int i = 0; i < n; i++) {
+            String output = message.randomMessage();
+            System.out.println(output);
+        }
+    }
 }
--- ObjectsAndClassesExercise/P02Articles.java
+++ ObjectsAndClassesExercise/P02Articles.java
@@ -1,4 +1,69 @@
 package ObjectsAndClassesExercise;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class P02Articles {
+    public static class Article {
+        private String title;
+        private String content;
+        private String author;
+
+        Article(String title, String content, String author) {
+            this.title = title;
+            this.content = content;
+            this.author = author;
+        }
+
+        public String getTitle() {
+            return this.title;
+        }
+
+        public String getContent() {
+            return this.content;
+        }
+
+        public String getAuthor() {
+            return this.author;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public String toString(){
+            return String.format("%s - %s: %s", this.title, this.content, this.author);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String[] input = scanner.nextLine().split(", ");
+        Article article = new Article(input[0],input[1],input[2]);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String[] command = scanner.nextLine().split(": ");
+            String temp = command[1];
+            if (command[0].contains("Edit")){
+                article.setContent(command[1]);
+            } else if (command[0].contains("ChangeAuthor")) {
+                article.setAuthor(command[1]);
+            } else if (command[0].contains("Rename")){
+                article.setTitle(command[1]);
+            }
+
+        }
+        System.out.println(article.toString());
+    }
 }
