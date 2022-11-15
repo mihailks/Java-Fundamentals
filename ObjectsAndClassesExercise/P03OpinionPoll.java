32ae010ad1ec4c9f542af17d3affe2c019cc9998
Mihail Slavov <inj.slavov@gmail.com>
2022-11-15 11:48:34 +0200

    -

--- ObjectsAndClassesExercise/P03OpinionPoll.java
+++ ObjectsAndClassesExercise/P03OpinionPoll.java
@@ -1,4 +1,53 @@
 package ObjectsAndClassesExercise;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class P03OpinionPoll {
+    public static class Person {
+        private String name;
+        private int age;
+
+        Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Person> personsList = new ArrayList<>();
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String[] input = scanner.nextLine().split(" ");
+            String name = input[0];
+            int age = Integer.parseInt(input[1]);
+
+            if (age > 30) {
+                Person person = new Person(name, age);
+                personsList.add(person);
+            }
+        }
+        for (Person person : personsList) {
+            System.out.println(person.getName() + " - " + person.getAge());
+        }
+    }
 }
--- ObjectsAndClassesExercise/P04Students.java
+++ ObjectsAndClassesExercise/P04Students.java
@@ -1,4 +1,59 @@
 package ObjectsAndClassesExercise;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
 public class P04Students {
+    public static class Student {
+        private String firstName;
+        private String lastName;
+        private double grade;
+
+        Student(String firstName, String lastName, double grade) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.grade = grade;
+        }
+
+        public String getFirstName() {
+            return this.firstName;
+        }
+
+        public String getLastName() {
+            return this.lastName;
+        }
+
+        public double getGrade() {
+            return this.grade;
+        }
+
+        public String toString() {
+            return String.format("%s %s: %.2f", this.firstName, this.lastName, this.grade);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        List<Student> studentList = new ArrayList<>();
+
+
+        for (int i = 0; i < n; i++) {
+            String[] input = scanner.nextLine().split(" ");
+            String firstName = input[0];
+            String lastName = input[1];
+            double grade = Double.parseDouble(input[2]);
+
+            Student student = new Student(firstName, lastName, grade);
+            studentList.add(student);
+        }
+        studentList.sort(Comparator.comparing(Student::getGrade).reversed());
+
+        for (Student student : studentList) {
+            System.out.println(student);
+        }
+    }
 }
