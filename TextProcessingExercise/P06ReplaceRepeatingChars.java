748953e0bdd5bfde81b097b7ff68cbdba24e6325
Mihail Slavov <inj.slavov@gmail.com>
2022-11-26 18:12:51 +0200

    -

--- TextProcessingExercise/P06ReplaceRepeatingChars.java
+++ TextProcessingExercise/P06ReplaceRepeatingChars.java
@@ -6,6 +6,14 @@ public class P06ReplaceRepeatingChars {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
+
+        for (int i = 0; i < input.length()-1; i++) {
+            if (input.charAt(i)!=input.charAt(i+1)){
+                System.out.print(input.charAt(i));
+            }
+        }
+        System.out.print(input.charAt(input.length()-1));
 
     }
 }
--- TextProcessingExercise/P08LettersChangeNumbers.java
+++ TextProcessingExercise/P08LettersChangeNumbers.java
@@ -6,6 +6,33 @@ public class P08LettersChangeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] input = scanner.nextLine().split("\\s+");
+        double totalSum = 0;
+        for (int i = 0; i < input.length; i++) {
+            String currentWord = input[i];
+            StringBuilder sb = new StringBuilder(currentWord);
+            sb.deleteCharAt(0);
+            sb.deleteCharAt(sb.length() - 1);
+            double number = Double.parseDouble(sb.toString());
+            char first = currentWord.charAt(0);
+            char last = currentWord.charAt(currentWord.length() - 1);
+            if (Character.isUpperCase(first)) {
+                int positionFirst = first - 65 + 1;
+                number /= positionFirst;
+            } else {
+                int positionFirst = first - 97 + 1;
+                number *= positionFirst;
+            }
+            if (Character.isUpperCase(last)) {
+                int positionSecond = last - 65 + 1;
+                number-=positionSecond;
+            } else {
+                int positionSecond = last - 97 + 1;
+                number+=positionSecond;
+            }
+            totalSum+=number;
 
+        }
+        System.out.printf("%.2f",totalSum);
     }
 }
