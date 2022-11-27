f4f6f735eaab10d04b5eaa4fe3ed6bd1afd02004
Mihail Slavov <inj.slavov@gmail.com>
2022-11-27 08:53:03 +0200

    -

--- TextProcessingMoreExercise/P05HTML.java
+++ TextProcessingMoreExercise/P05HTML.java
@@ -6,7 +6,26 @@ public class P05HTML {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String title = scanner.nextLine();
+        String content = scanner.nextLine();
+        String comments = scanner.nextLine();
 
+        System.out.println("<h1>");
+        System.out.println("    " + title);
+        System.out.println("</h1>");
+
+        System.out.println("<article>");
+        System.out.println("    " + content);
+        System.out.println("</article>");
+
+        while (!comments.equals("end of comments")){
+
+            System.out.println("<div>");
+            System.out.println("    " + comments);
+            System.out.println("</div>");
+
+            comments = scanner.nextLine();
+        }
 
     }
 }
